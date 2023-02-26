ef19089096c163ffbc4f47d2ff737f192e6a1f13
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-02-26 20:59:59 +0000

    Rubocop config and tidyup

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :team do
-    
+    team_type { 'mens' }
+    name { Faker::Sports::Football.team }
+    ds { Faker::Name.name }
+    riders { "#{Faker::Name.name}, #{Faker::Name.name}" }
   end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Team, type: :model do
+RSpec.describe Team do
   pending "add some examples to (or delete) #{__FILE__}"
 end
