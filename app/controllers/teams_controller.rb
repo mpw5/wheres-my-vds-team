fb925e502f30c02456f683878ae57102e7dd37a2
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-03-01 10:27:54 +0000

    Use scopes to select races

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,38 +3,12 @@
 class TeamsController < ApplicationController
   def index
     @team_ds = permitted_params[:team_ds]
-    teams
-    mens_races
-    womens_races
+    @teams = Team.where(ds: @team_ds).or(Team.where(name: @team_ds))
   end
 
   private
 
   def permitted_params
     params.permit(:team_ds)
-  end
-
-  def teams
-    @teams ||= Team.where(ds: @team_ds).or(Team.where(name: @team_ds))
-  end
-
-  def mens_races
-    @mens_races ||= Race.where(race_type: 'mens').and(
-      Race.where('end_date >= ?', Time.zone.today).or(
-        Race.where('start_date >= ?', Time.zone.today).and(
-          Race.where(end_date: nil)
-        )
-      )
-    ).first(10)
-  end
-
-  def womens_races
-    @womens_races ||= Race.where(race_type: 'womens').and(
-      Race.where('end_date >= ?', Time.zone.today).or(
-        Race.where('start_date >= ?', Time.zone.today).and(
-          Race.where(end_date: nil)
-        )
-      )
-    ).first(10)
   end
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class Race < ApplicationRecord
+  scope :upcoming_mens_races, lambda {
+    where(race_type: 'mens').and(
+      Race.where('end_date >= ?', Time.zone.today).or(
+        Race.where('start_date >= ?', Time.zone.today).and(
+          Race.where(end_date: nil)
+        )
+      )
+    ).first(10)
+  }
+
+  scope :upcoming_womens_races, lambda {
+    where(race_type: 'womens').and(
+      Race.where('end_date >= ?', Time.zone.today).or(
+        Race.where('start_date >= ?', Time.zone.today).and(
+          Race.where(end_date: nil)
+        )
+      )
+    ).first(10)
+  }
 end
