530df19b54542a710a82eff3c99ca54f0bbad031
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-03-01 10:33:41 +0000

    DRY race scopes

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
 
 class Race < ApplicationRecord
-  scope :upcoming_mens_races, lambda {
-    where(race_type: 'mens').and(
-      Race.where('end_date >= ?', Time.zone.today).or(
-        Race.where('start_date >= ?', Time.zone.today).and(
-          Race.where(end_date: nil)
-        )
-      )
-    ).first(10)
-  }
-
-  scope :upcoming_womens_races, lambda {
-    where(race_type: 'womens').and(
+  scope :upcoming_races, lambda { |race_type|
+    where(race_type:).and(
       Race.where('end_date >= ?', Time.zone.today).or(
         Race.where('start_date >= ?', Time.zone.today).and(
           Race.where(end_date: nil)
