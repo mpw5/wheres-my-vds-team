84ccb0223167ce3af34f130d288a2b0ac458e31b
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2025-02-26 17:07:32 +0000

    Add 2025 teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,7 +7,7 @@ class Team < ApplicationRecord
 
   def riders_array
     parsed_riders = []
-    raw_riders = riders&.split(', ')&.sort
+    raw_riders = riders&.split(',')&.sort
     raw_riders&.each do |rider|
       parsed_riders << I18n.transliterate(rider).downcase
     end
