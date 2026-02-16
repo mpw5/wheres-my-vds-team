6b9b57f57949eef4b5ca449bcbf8ffcca65af620
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2026-02-16 11:51:07 +0000

    Adjust scraper request

--- spec/services/startlist_scraper_service_spec.rb
+++ spec/services/startlist_scraper_service_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe StartlistScraperService do
   end
 
   before do
-    allow(URI).to receive(:open).with("https://#{fake_url}")
+    allow(URI).to receive(:open).with("https://#{fake_url}", { 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' })
     allow(Nokogiri::HTML::Document).to receive(:parse).and_return(fake_html)
   end
 
