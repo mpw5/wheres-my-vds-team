8d0ed015b149601d7fc4807c270cbfef1c8a5f5e
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2026-02-17 09:00:56 +0000

    More db setup debugging

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,6 +2,8 @@
 
 class TeamsController < ApplicationController
   def index
+    Rails.logger.info "DB CONFIG: #{ActiveRecord::Base.connection_db_config.configuration_hash}"
+    Rails.logger.info "DB TABLES: #{ActiveRecord::Base.connection.tables.join(', ')}"
     @team_ds = permitted_params[:team_ds]&.strip
     @teams = Team.teams_for @team_ds
   end
