0d929a6ef45a0ff7c5668a210b12734bd54600a2
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2026-02-17 15:55:57 +0000

    Refactor riders_array method

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,11 +6,6 @@ class Team < ApplicationRecord
   }
 
   def riders_array
-    parsed_riders = []
-    raw_riders = riders&.split(',')&.sort
-    raw_riders&.each do |rider|
-      parsed_riders << I18n.transliterate(rider).downcase
-    end
-    parsed_riders
+    riders&.split(',')&.sort&.map { |rider| I18n.transliterate(rider).downcase } || []
   end
 end
